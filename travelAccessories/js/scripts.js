5949598c2262a3d17ddae14b81d2bbeeb18a5124
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-02-06 20:45:45 +0200

    menu added

--- travelAccessories/js/scripts.js
+++ travelAccessories/js/scripts.js
@@ -3,14 +3,40 @@ $(document).ready(function() {
     // Adaptive menu toggle
     // ==========================================================
 
+    $('.adaptive-manu-toggle').on('click', function(e) {
+        e.preventDefatul();
+        $(this).toggleClass('adaptive-menu-toggle--open');
+        $('.adaptive-menu-container').toggleClass('adaptive-menu-container--open');
+    });
+
     // Add active class to header menu after click
     // ==========================================================
+
+    $('.header-menu__link').on('click', function(e) {
+        e.preventDefault();
+        $('.header-menu__link').removeClass('header-menu__link--active');
+        $(this).addClass('header-menu__link--active');
+    });
 
     // Add active class to header adaptive menu af.click
     // ==========================================================
 
+    $('.adaptive-menu__link').on('click', function(e) {
+        e.preventDefault();
+        $('.adaptive-menu__link').removeClass('adaptive-menu__link--active');
+        $(this).addClass('adaptive-menu__link--active');
+    })
+
     // Add class to header after scroll > 300 px
     // ==========================================================
+
+    $(window).on('scroll', function() {
+        if($(this).scrollTop() > 300) {
+            $('.header').addClass('header--scrolled');
+        } else {
+            $('.header').removeClass('header--scrolled');
+        }
+    });
 
     // Scroll to target button
     // ==========================================================
